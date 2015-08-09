18f368817347cd419b54adcf3f377acbf0e89d8a
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-08-09 20:53:49 -0300

    Changing field name from parent to parents

--- lib/page.rb
+++ lib/page.rb
@@ -1,13 +1,13 @@
 require 'nokogiri'
 
 class Page
-  attr_accessor :title, :body, :children, :parent 
+  attr_accessor :title, :body, :children, :parents
   
-  def initialize(title, body, children, parent)
+  def initialize(title, body, children, parents)
     @title = title
     @body = body
     @children = children
-    @parent = parent
+    @parents = parents
   end
 
   def bindings
