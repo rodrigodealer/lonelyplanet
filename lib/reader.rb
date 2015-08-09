ffad52c672406745f96fedce987280f6a9282c52
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-08-09 20:57:15 -0300

    Adding Reader class - It receives a destination file as parameter, a taxonomy file as parameter and combine them and process a batch of Destination pages

---
+++ lib/reader.rb
@@ -0,0 +1,46 @@
+require 'parser'
+require 'html_creator'
+
+class Reader
+  attr_accessor :destinations_document
+  attr_accessor :taxonomies_document
+  
+  def initialize(destinations_file, taxonomies_file)
+    @destinations_document = Parser.new(destinations_file).document
+    @taxonomies_document   = Parser.new(taxonomies_file).document
+  end
+  
+  def destinations
+    destinations_document.xpath('//destinations//destination')
+  end
+  
+  def taxonomies
+    taxonomies_document.xpath('//taxonomies//taxonomy//node')
+  end
+  
+  def process
+    destinations.each {|destination|
+      title = destination["title"]
+      body = destination.css('introductory introduction overview').text
+      children = taxonomies_children_of(title).css('node_name').to_a.map{|child| "#{child.text}" } unless taxonomies_children_of(title).nil?
+      parents = parents_of(taxonomies.at_xpath("node_name[text()='#{title}']").parent.parent.css('node_name').first, [])
+      page = Page.new(title, body, children, parents)
+      creator = HtmlCreator.new("output/#{title}.html", "spec/samples/destination.html.erb")
+      creator.write(page.bindings)
+    }
+  end
+    
+  private
+  def parents_of(title, parents)
+    parents << title.text
+    if title.parent.parent.at_xpath('node_name').nil?
+      parents
+    else
+      parents_of(title.parent.parent.at_xpath('node_name'), parents)
+    end
+  end
+  
+  def taxonomies_children_of(title)
+    taxonomies.at_xpath("node_name[text()='#{title}']").next_element
+  end
+end
---
+++ spec/unit/reader_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'reader'
+
+RSpec.describe Reader do
+  let(:destinations_file) { "spec/samples/destinations.xml" }
+  let(:taxonomies_file) { "spec/samples/taxonomy.xml" }
+  let(:reader) { Reader.new(destinations_file, taxonomies_file) }
+  describe  '#initialize' do    
+    it 'has 24 destinations in document' do
+      expect(reader.destinations_document.xpath("//destinations//destination").size).to be_eql(24)
+    end
+    
+    it 'has 24 taxonomies in document' do
+      expect(reader.taxonomies_document.xpath("//taxonomies//taxonomy//node").size).to be_eql(24)
+    end
+  end
+  
+  describe '#destinations' do
+    it 'returns same content size in destinations and destinations_document' do
+      expect(reader.destinations.size).to be_eql(reader.destinations_document.xpath("//destinations//destination").size)
+    end
+  end
+  
+  describe '#taxonomies' do
+    it 'returns same content size in taxonomies and taxonomies_document' do
+      expect(reader.taxonomies.size).to be_eql(reader.taxonomies_document.xpath("//taxonomies//taxonomy//node").size)
+    end
+  end
+end
