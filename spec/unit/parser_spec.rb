91fa01dc511156107e197721d404dda9b46b8248
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-08-10 19:38:24 -0300

    Adding spec for initialization error scenario

--- spec/unit/parser_spec.rb
+++ spec/unit/parser_spec.rb
@@ -4,17 +4,17 @@ require 'parser'
 RSpec.describe Parser do
   describe  '#initialize' do
     let(:parser) { Parser.new('spec/samples/taxonomy.xml') }
-    
+
     it 'should initialize and load a file' do
       expect(parser.document).to_not be_nil
     end
-    
+
     it 'should get a list of 24 taxonomies' do
       expect(parser.document.xpath("//taxonomies//taxonomy//node").size).to be_eql(24)
     end
-  end
-  
-  describe '#parse' do
-    let(:parser) { Parser.new('spec/samples/destinations.xml') }
+
+    it 'should try to initialize and return an error' do
+      expect { Parser.new('spec/samples/taxonomy.txt') }.to raise_error(StandardError)
+    end
   end
 end
